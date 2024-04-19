338a2c73bc99742b873b4bb952acc7efd3294065
Tehmina Saleem <104255800+Tehmina-Saleem@users.noreply.github.com>
2024-04-19 15:02:18 +0500

    "updated rendering of menu component"

--- src/Pages/Menu/Menu.jsx
+++ src/Pages/Menu/Menu.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 import Dashboard from "../../Components/Dashboard";
+import Users from "../../Components/Users";
 function Menu() {
   return (
     <>
       <Dashboard />
+   
     </>
   );
 }
