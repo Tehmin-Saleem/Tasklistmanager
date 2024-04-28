9282b4dd64b670f31d9e04ad6b130dffc6ac5efa
Tehmina Saleem <104255800+Tehmina-Saleem@users.noreply.github.com>
2024-04-28 22:39:51 +0500

    "Update:Updated dashboard"

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -7,6 +7,7 @@ import { IoNotificationsOutline } from "react-icons/io5";
 import Calendar from 'react-calendar';
 import ProgressBar from "@ramonak/react-progress-bar";
 import 'react-calendar/dist/Calendar.css';
+import {Link} from "react-router-dom";
 function Dashboard() {
   const [date, setDate] = useState(new Date());
 
@@ -100,32 +101,33 @@ function Dashboard() {
     };
   }, []);
   return (
-    <div className="grid grid-rows-3 grid-flow-col bg-gray-100">
-    <div className="topleft row-span-3 col-span-1 bg-white h-full pl-8 mr-4">
-        <span>
-          <img src={group} alt="Logo" className="mr-2 pt-4" />
-          <h2 className="text-2xl font-medium ml-6 font-bold text-[#4BCBEB]">
+    <div className=" w-full  grid grid-rows-3 grid-flow-col bg-slate-100">
+    <div className="topleft row-span-3 bg-[#FFFFFF] w-[320px] shadow-xl">
+        <span className="flex items-center m-3 p-3 ">
+          <img src={group} alt="Logo" className="mr-2 px-2" />
+          <h2 className="text-2xl font-medium  font-bold text-[#4BCBEB]">
             Task Manager List
           </h2>
           <hr className="ml-1 mr-4" />
         </span>
-        <h2 className="text-black font-bold mt-8">MENU</h2>
-        <button className="flex flex-col pr-[100px] py-2 mt-4 hover:shadow-md">
-          <h2 className="text-[#4BCBEB] ml-2 font-bold">Dashboard</h2>
+        <h2 className="text-black text-lg pl-6 pt-5 font-bold">MENU</h2>
+        <div className="m-4 text-lg pl-6 p-3 font-bold text-[#4BCBEB] hover:shadow-md rounded-xl ">
+          <Link to="/Dashboard">Dashboard</Link>
+        </div>
+        <div className="m-4 text-lg pl-6 p-3  hover:shadow-md rounded-xl">
+          <Link to="/Users">Users</Link>
+        </div>
+        <button div className="m-4 text-lg pl-6 p-3 pr-5 hover:shadow-md rounded-xl ">
+          <Link to="/tasks">Tasks</Link>
         </button>
-        <button className="flex flex-col hover:text-[#4BCBEB] pr-[140px] hover:shadow-md py-2 mt-4">
-          <h2 className="font-bold ml-2">Users</h2>
-        </button>
-        <button className="flex flex-col hover:text-[#4BCBEB] pr-[140px] hover:shadow-md py-2 mt-4">
-          <h2 className="font-bold ml-2">Tasks</h2>
-        </button>
-        <button className="flex flex-col hover:text-[#4BCBEB] pr-[120px] hover:shadow-md py-2 mt-4">
-          <h2 className="font-bold ml-2">Settings</h2>
-        </button>
+        <div className="m-4 text-lg pl-6 p-3 hover:shadow-md rounded-xl">
+          <Link to="/settings">Settings</Link>
+        </div>
+
       </div>
      
         <div className="topright col-span-4 flex  bg-white h-24">
-          <h1 className="font-bold text-3xl ml-1 mt-8">Dashboard</h1>
+          <h1 className="font-bold text-3xl ml-7 mt-8">Dashboard</h1>
           <div className="ml-auto flex items-center space-x-4">
             <IoNotificationsOutline className="text-gray-400 size-[2rem]" />
             <CgProfile className="text-gray-400 size-[2rem]" />
@@ -135,31 +137,39 @@ function Dashboard() {
             </div>
           </div>
         </div>
-        <div className="centerdiv bg-white ml-4 flex grid grid-cols-4 col-span-4 h-full border border-rounded">
+        <section className="bg-white row-span-2 col-span-2 m-3 ">
   <h1 className="font-bold text-lg  col-span-4">Analytics</h1>
   <hr className="col-span-4" />
 
-  <div className="firstdiv bg-[#F4F2FF] flex-row mt-4 h-auto  border-rounded ml-2 pb-3 mr-3 col-span-1">
-    <h1 className="text-black font-bold pb-3  mt-4 ml-4 ">Total Tasks</h1>
-    <h1 className="text-lg font-bold mx-4 pb-3 mb-3 text-gray-600">90/100</h1>
+  <section className="  grid grid-cols-4 gap-4">
+  <div className=" bg-[#F4F2FF]  m-4 rounded-2xl  p-4">
+    <h1 className="text-black font-bold    ">Total Tasks</h1>
+    <h1 className="text-lg font-bold mx-4 pb-6 mb-3 pt-3 text-gray-600">90/100</h1>
     <div className="mx-3 text-blue-100 mb-7"><ProgressBar completed="90" /></div>
   </div>
-  <div className="seconddiv bg-[#E2EFFC] flex-row mt-4 h-auto pb-3 mr-3 col-span-1">
-    <h1 className="text-black font-bold pb-3 mt-4 ml-4">Completed Tasks</h1>
-    <h1 className="text-lg font-bold mx-4 pb-3 mb-3 text-gray-600">80/100</h1>
-    <div className="mx-3 mb-7 mt-3"><ProgressBar completed="80" /></div>
+  <div className=" bg-[#E2EFFC]  m-4 rounded-2xl  p-4">
+    <h1 className="text-black font-bold    ">Completed Tasks</h1>
+    <h1 className="text-lg font-bold mx-4 pb-6 mb-3 pt-3 text-gray-600">80/100</h1>
+    <div className="mx-3 text-blue-100 mb-7"><ProgressBar completed="90" /></div>
   </div>
-  <div className="3rddiv bg-[#FBEDD2] flex-row h-auto mt-4 pb-3 mr-3 col-span-1">
+  <div className="3rddiv bg-[#FBEDD2] m-4 rounded-2xl p-4">
     <h1 className="text-black font-bold mt-4 ml-4">Pending Tasks</h1>
     <h1 className="text-lg font-bold mx-4 pb-3 mb-3 text-gray-600 mt-3">50/100</h1>
     <div className="mx-3 mb-7 mt-3"><ProgressBar completed="50" /></div>
   </div>
-  <div className="4thdiv bg-[#E0F6F4] flex-row h-auto mt-4 pb-5 mr-8 col-span-1">
+  <div className="4thdiv bg-[#E0F6F4] m-4 rounded-2xl p-4">
     <h1 className="text-black font-bold mt-4 ml-4">Decline Tasks</h1>
     <h1 className="text-lg font-bold mx-4 pb-3 mb-3 text-gray-600 mt-3">10/100</h1>
     <div className="mx-3 mb-7 mt-3"><ProgressBar completed="10" /></div>
   </div>
-  <h1 className="font-bold text-2xl mt-[90px]">Total Task Ratio</h1>
+</section>
+ 
+  <section className=" bg-slate-300 grid grid-cols-2  col-auto">
+            <div className="flex-1 bg-white p-4 ">
+              <div className=" flex">
+                <h1 className="text-2xl font-bold p-6 m-1">
+                  Total Task Ratio{" "}
+                </h1>
   <span className="dropdownbutton text-[#4BCBEB] pl-[100px] font-bold mt-[90px]">
           <select value={selectedOption} onChange={handleSelectChange}>
             <option value="Weekly">Weekly</option>
@@ -167,21 +177,60 @@ function Dashboard() {
             <option value="Daily">Daily</option>
           </select>
         </span>
-       
-</div>
+       </div>
+       {/* <div className=" h-60  w-auto ">
+                <Line
+                  data={ctx}
+                  options={{
+                    scales: {
+                      x: {
+                        display: true,
+                        title: {
+                          display: true,
+                        },
+                        grid: {
+                          display: false, // Hide x-axis grid lines
+                        },
+                      },
+                      y: {
+                        display: true,
+                        title: {
+                          display: true,
+                        },
+                        grid: {
+                          display: true, // Hide y-axis grid lines
+                        },
+                        ticks: {
+                          beginAtZero: false,
+                        },
+                      },
+                    },
+                    plugins: {
+                      legend: {
+                        display: false, // Hide legend
+                      },
+                    },
+                    responsive: true,
+                  }}
+                />
+              </div> */}
+            </div>
+
 <div className="bg-white"> 
+<div className=" bg-white pl-[80px]">
   <canvas ref={chartRef}/>
-  </div>
-  <div className="w-[550px] bg-white pl-[150px]"><Calendar
+ 
+<Calendar
         onChange={onChange}
         value={date}
       /></div>
-  
-
-        </div>
+   </div>
+</section>
+     </section>  
           
-           
-   
+         
+           </div>
+  
   );
 }
 
